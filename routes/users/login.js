bf6fb37f079817297992542d0ac020ef515d052c
Thomas Claessens <tom@itcl.io>
2022-05-11 19:36:46 +0200

    ...

--- routes/users/login.js
+++ routes/users/login.js
@@ -7,16 +7,13 @@ const router = express.Router();
 router.use(express.json())
 router.post('/', async function (req, res, next) {
   const apiKey = req.headers["apikey"];
-  console.log("aaaa",req.headers)
   if (apiKey == null) {
-    console.log("bbbb")
     return res.status(400).json({ error: 'apikey not present' });
   }
   const payload = req.body;
   try {
     const u = new User();
     const user = await u.find(payload);
-    console.log("user",user)
     if (user && user.active) {
       if (user && user.password && user.password === CryptoJS.MD5(JSON.stringify(payload.password)).toString()) {
         const o = {
@@ -28,10 +25,8 @@ router.post('/', async function (req, res, next) {
           date_created: user.date_created
         }
         const token = jwt.sign(o, process.env.API_SECRET, { expiresIn: "20m" })
-        console.log("token",token)
         res.json({user:user, accessToken: token});
       } else {
-        console.log("rrrrrrpassword not correct")
         res.json({ error: 'password not correct' });
       }
     } else {
